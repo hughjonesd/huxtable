e1b8861b10dce2d1066430af88541a1dc4ea4159
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-04 15:21:27 +0000

    better testing of examples

--- tests/testing-functions.R
+++ tests/testing-functions.R
@@ -1,18 +1,24 @@
 
 # runs example and checks it hasn't changed
 test_ex_same <- function(fname) {
-  path <- devtools::package_file('man', paste0(fname, '.Rd'))
+  path <- devtools::find_topic(fname)
+  if (is.null(path)) stop('No help exists for ', fname)
   expath <- tempfile('test-example')
   tools::Rd2ex(path, expath, commentDontrun = TRUE, commentDonttest = TRUE)
+  if (! file.exists(expath)) stop('Could not make example for ', fname)
   excode <- parse(expath)
   exenv <- new.env()
   exres <- list()
   i <- 1
   for (expr in excode) {
-    capture.output(res <- eval(expr, envir = exenv))
+    out <- capture.output(res <- eval(expr, envir = exenv))
+    rds_name <- paste0(fname, '-example-value-', i, '.rds')
+    fp <- file.path(test_path(), rds_name)
+    expect_equal_to_reference(res, fp, info = paste0('Expression changed value: ', deparse(expr)))
     rds_name <- paste0(fname, '-example-output-', i, '.rds')
     fp <- file.path(test_path(), rds_name)
-    expect_equal_to_reference(res, fp, info = paste0('Changed expression: ', deparse(expr)))
+    expect_equal_to_reference(out, fp, info = paste0('Expression changed output: ', deparse(expr)))
+
     i <- i + 1
   }
 }
