1cdc8c14b176570d7522321ef080b26b3e73dd7f
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-05 02:57:43 +0000

    test

--- tests/testthat/test-object-manipulation.R
+++ tests/testthat/test-object-manipulation.R
@@ -3,6 +3,8 @@ context("Object manipulation")
 
 source(devtools::package_file('tests', 'testing-functions.R'))
 
+ht <- huxtable(a = 1:3, b = 1:3)
+
 test_that('Object creation works', {
   test_ex_same('huxtable')
 })
@@ -12,6 +14,11 @@ test_that('Object subsetting and replacement work', {
   test_ex_same('add_colnames')
   test_ex_same('cbind.huxtable')
   test_ex_same('t.huxtable')
+})
+
+test_that('Subsetting preserves rownames', {
+  rownames(ht) <- letters[1:3]
+  expect_equal(rownames(ht[1:2,]), letters[1:2])
 })
 
 
