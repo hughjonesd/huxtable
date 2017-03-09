cfa1fd5b50111f96980218f9cff3d37286da92f5
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-09 11:05:04 +0000

    trivia

--- build-cran.R
+++ build-cran.R
@@ -11,12 +11,10 @@ for (f in list.files('vignettes', pattern = '.*\\.Rmd$')) {
   if (length(out) > 0) stop('vignettes and docs Rmd files differ, please fix!')
 }
 
-v <- devtools::as.package('.')$version
+
 devtools::build()
 chk <- devtools::check(env_vars = c('RSTUDIO_PANDOC' = '/Applications/RStudio.app/Contents/MacOS/pandoc'),
       document = FALSE, check_version = TRUE)
-# system("R", args = c('CMD', 'CHECK', '--as-cran', huxtar),
-#      env = c('RSTUDIO_PANDOC' = '/Applications/RStudio.app/Contents/MacOS/pandoc'))
 if (length(chk$errors) > 0 || length(chk$warnings) > 0) {
   cat('\n\nR CMD CHECK errors:\n')
   cat(chk$errors)
@@ -27,16 +25,16 @@ if (length(chk$errors) > 0 || length(chk$warnings) > 0) {
 if (length(chk$notes)) {
   cat('R CMD CHECK notes:\n')
   cat(chk$notes)
-  cat('Tag this version? y[n]')
+  cat('\nTag this version? y[n]')
   yn <- readLines(con="stdin", 1)
-  if (! yn %in% c('Y', 'y')) stop('OK, stopping')
+  if (! yn %in% c('Y', 'y')) stop('OK, stopping.')
 }
 
-newtag <- paste0('v', v, '-rc')
+newtag <- paste0('v', devtools::as.package('.')$version, '-rc')
 tags <- tags()
 tags <- grep(newtag, names(tags), fixed = TRUE, value = TRUE)
 tags <- as.numeric(gsub(newtag, '', tags))
 rc <- if (length(tags)) as.integer(max(tags) + 1) else 1L
 newtag <- paste0(newtag, rc)
-cat('\nTagging current version as: ', newtag)
+cat('\nTagging current version as: ', newtag, '\n')
 tag(repository('.'), newtag, message = paste('CRAN release candidate for', v))
