959cedc985b82ac845948b58392aeec63134b515
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-10 21:50:17 +0000

    move slow pandoc tests to file so they run late

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -24,27 +24,13 @@ test_that('Screen output examples unchanged', {
   test_ex_same('to_screen')
 })
 
+
 test_that('Multi-rowspan screen output is sane', {
   ht <- hux(a = rep('aaaaaa', 10), b = rep('bbbbbb', 10))
   rowspan(ht)[1,1] <- 10
   expect_equal_to_reference(to_screen(ht), 'multirow.rds')
 })
 
-test_that('Four spaces does not cause <pre><code> markup', {
-  #skip('Waiting for knitr fix')
-  skip_without_pandoc()
-  on.exit(if (exists('output')) file.remove(output))
-  output <- rmarkdown::render('fourspace-html-test.Rmd', quiet = TRUE)
-  lines <- readLines(output)
-  expect_false(any(grepl('findme&lt;/td&gt;', lines)))
-})
-
-test_that('Row heights do not screw up latex multicol', {
-  skip_without_pandoc()
-  on.exit(if (exists('output')) file.remove(output))
-  expect_silent(output <- rmarkdown::render('rowheight-multicol-test.Rmd', quiet = TRUE))
-
-})
 
 test_that('guess_knitr_output_format() gets it right', {
   out <- character(0)
---
+++ tests/testthat/test-with-pandoc.R
@@ -0,0 +1,22 @@
+
+context('Test output using pandoc')
+
+
+source('functions.R')
+
+
+test_that('Four spaces does not cause <pre><code> markup', {
+  #skip('Waiting for knitr fix')
+  skip_without_pandoc()
+  on.exit(if (exists('output')) file.remove(output))
+  output <- rmarkdown::render('fourspace-html-test.Rmd', quiet = TRUE)
+  lines <- readLines(output)
+  expect_false(any(grepl('findme&lt;/td&gt;', lines)))
+})
+
+
+test_that('Row heights do not screw up latex multicol', {
+  skip_without_pandoc()
+  on.exit(if (exists('output')) file.remove(output))
+  expect_silent(output <- rmarkdown::render('rowheight-multicol-test.Rmd', quiet = TRUE))
+})
