31eb46ef9ec0dcd198cdcb51875fd77d2314bfd9
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-13 13:31:30 +0000

    huxreg tests, docs

---
+++ tests/testthat/test-huxreg.R
@@ -0,0 +1,17 @@
+
+
+context('huxreg')
+
+test_that('huxreg examples unchanged', {
+  test_ex_same('huxreg')
+})
+
+test_that('huxreg copes with different models', {
+  set.seed(27101975)
+  dfr <- data.frame(a = rnorm(100), b = rnorm(100))
+  dfr$y <- dfr$a + rnorm(100)
+  lm1 <- lm(y ~ a, dfr)
+  lm2 <- lm(y ~ a + b, dfr)
+  glm1 <- glm1(I(y > 0) ~ a, dfr, family = binomial)
+  expect_silent(hr <- huxreg(lm1, lm2, glm1))
+})
