555f090a9800118dc70cfe94a1afdf11f2dee6be
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-14 16:56:28 +0000

    test bugfix and adding example-rds back to git on this branch too

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -45,9 +45,9 @@ test_that('Row heights do not screw up latex multicol', {
   if (exists('output')) file.remove(output)
 })
 
-test_that('Vignettes build without pandoc',
+test_that('Vignettes build without pandoc',{
   skip('Too hard to make file paths work in automated testing')
   with_mock(`rmarkdown::pandoc_available` = function (...) FALSE, {
     expect_error(rmarkdown::render('design-principles.Rmd'), regexp = NA) # expect no error
   })
-)
+})
