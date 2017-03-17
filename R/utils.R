174af137458883a979d23bd547f1f0c2f1a816f6
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-17 22:37:34 +0000

    add_footnote utility function

--- R/utils.R
+++ R/utils.R
@@ -184,6 +184,34 @@ guess_knitr_output_format <- function() {
   of
 }
 
+#' Add a row with a footnote
+#'
+#' This adds a single row at the bottom. The first cell contains the footnote; it spans
+#' the entire table and has a border above only.
+#' @param ht A huxtable.
+#' @param text Text for the footnote.
+#' @param ... Other properties, passed to \code{\link{set_cell_properties}} for the footnote cell.
+#'
+#' @return The modified huxtable
+#' @export
+#'
+#' @examples
+#' ht <- hux(a = 1:5, b = 1:5, d = 1:5)
+#' ht <- add_footnote(ht, '* this is a footnote')
+#' ht
+add_footnote <- function(ht, text, italic = FALSE, ...) {
+  nr <- nrow(ht)
+  nc <- ncol(ht)
+  ht <- rbind(ht, rep('', nc), copy_cell_props = FALSE)
+  ht[nr, 1] <- text
+  colspan(ht)[nr, 1] <- nc
+  ht <- set_all_borders(ht, nr, 1, 0)
+  set_top_border(ht, nr, 1, 1)
+  wrap(ht)[nr, 1] <- TRUE
+  if (! missing(...)) ht <- set_cell_properties(ht, nr, 1, ...)
+
+  ht
+}
 
 #' Huxtable logo
 #'
