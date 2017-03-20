efbd1c0b2c9fa2fc01d5b706e0461b882b257bfd
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-20 00:03:22 +0000

    bugfix HTML decimal padding

--- R/utils.R
+++ R/utils.R
@@ -85,7 +85,7 @@ decimal_pad <- function(col, pad_chars, type) {
 
   pad_to <- max(chars_after_.) - chars_after_.
   pad_char <- if (type == 'html') '&nbsp;' else ' '
-  col <- paste0(col, str_rep(' ', pad_to))
+  col <- paste0(col, str_rep(pad_char, pad_to))
 
   orig_col[! na_pad] <- col
   orig_col
