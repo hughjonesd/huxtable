230c72c8c989537b1fd1d538056c7fbb991a0f45
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-29 14:28:27 +0100

    new themes tests

---
+++ tests/testthat/test-themes.R
@@ -0,0 +1,21 @@
+
+context('Themes')
+
+
+source('functions.R')
+
+
+test_that('Themes work', {
+  ht <- huxtable(a = 1:5, b = 1:5)
+  expect_error(theme_basic(ht), regexp = NA)
+  expect_error(theme_article(ht), regexp = NA)
+  expect_error(theme_striped(ht), regexp = NA)
+})
+
+
+test_that('Themes work with options', {
+  ht <- huxtable(a = 1:5, b = 1:5)
+  expect_error(theme_basic(ht, header_row = FALSE, header_col = FALSE), regexp = NA)
+  expect_error(theme_striped(ht, stripe = 'purple', header_row = FALSE, header_col = FALSE), regexp = NA)
+  expect_error(theme_article(ht, header_row = FALSE, header_col = FALSE), regexp = NA)
+})
