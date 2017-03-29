50d7990e049354f237a1c79ac91a212f776362fc
David Hugh-Jones <davidhughjones@gmail.com>
2017-03-29 14:28:49 +0100

    trivia

--- scripts/run-covr.R
+++ scripts/run-covr.R
@@ -5,4 +5,5 @@ Sys.setenv(COVERAGE='yes')
 all_created <- c(huxtable:::huxtable_cell_attrs, huxtable:::huxtable_col_attrs,
       huxtable:::huxtable_row_attrs, huxtable:::huxtable_table_attrs)
 all_created <- paste0('^', all_created)
-package_coverage(function_exclusions = all_created)
+cov <- package_coverage(function_exclusions = all_created)
+Sys.unsetenv('COVERAGE')
