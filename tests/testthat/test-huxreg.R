f98fdc7cedefc91f0c13fe2931acb7cf9997b8c3
David Hugh-Jones <davidhughjones@gmail.com>
2017-04-04 23:21:34 +0100

    add test for huxreg confidence intervals

--- tests/testthat/test-huxreg.R
+++ tests/testthat/test-huxreg.R
@@ -6,6 +6,7 @@ test_that('huxreg examples unchanged', {
   test_ex_same('huxreg')
 })
 
+
 test_that('huxreg copes with different models', {
   set.seed(27101975)
   dfr <- data.frame(a = rnorm(100), b = rnorm(100))
@@ -14,4 +15,17 @@ test_that('huxreg copes with different models', {
   lm2 <- lm(y ~ a + b, dfr)
   glm1 <- glm(I(y > 0) ~ a, dfr, family = binomial)
   expect_silent(hr <- huxreg(lm1, lm2, glm1))
+})
+
+
+test_that('huxreg confidence intervals work', {
+  set.seed(27101975)
+  dfr <- data.frame(a = rnorm(100), b = rnorm(100))
+  dfr$y <- dfr$a + rnorm(100)
+  lm1 <- lm(y ~ a, dfr)
+  lm2 <- lm(y ~ a + b, dfr)
+  glm1 <- glm(I(y > 0) ~ a, dfr, family = binomial)
+  library(nnet)
+  mn <- nnet::multinom(I(y > 0) ~ a, dfr, trace = FALSE)
+  expect_silent(hr <- huxreg(lm1, lm2, glm1, mn, error_style = "ci", statistics = c('r.squared')))
 })
