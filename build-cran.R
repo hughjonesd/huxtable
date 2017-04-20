c412f575f43b1dbaa8389916596e93eda15cbbb0
David Hugh-Jones <davidhughjones@gmail.com>
2017-04-20 23:28:08 +0100

    update build script

--- build-cran.R
+++ build-cran.R
@@ -3,6 +3,7 @@
 library(devtools)
 library(git2r)
 
+setwd('~/huxtable')
 
 # Check git is up to date -----------------------------------------------------------------------------------------
 
@@ -20,28 +21,10 @@ for (f in list.files('vignettes')) {
 }
 
 
-# Build all kinds of vignettes in inst/doc ------------------------------------------------------------------------
-
-# file.remove(list.files('inst/doc', full.names = TRUE))
-# setwd('vignettes')
-# for (f in list.files(pattern = '*.Rmd')) {
-#   output_fs <- rmarkdown::render(f, output_format = 'all')
-#
-#   r_code_f <- knitr::purl(f)
-#   for (output_f in c(f, r_code_f, output_fs)) {
-#     if (file.copy(output_f, file.path('..', 'inst', 'doc'))) {
-#       if (output_f != f) file.remove(output_f)
-#     } else {
-#       warning("Could not copy output file ", output_f, ' to inst/doc')
-#     }
-#   }
-# }
-# setwd('..')
-
 
 # Run R CMD check -------------------------------------------------------------------------------------------------
 
-
+# this automatically builds the package
 chk <- devtools::check(env_vars = c('RSTUDIO_PANDOC' = '/Applications/RStudio.app/Contents/MacOS/pandoc'),
       document = FALSE, check_version = TRUE)
 if (length(chk$errors) > 0 || length(chk$warnings) > 0) {
