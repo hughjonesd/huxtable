fa0a880b9924e56e55985f29f330c5d99ee50fa2
David Hugh-Jones <davidhughjones@gmail.com>
2017-04-23 08:17:01 +0100

    Document mutate not mutate_

--- R/dplyr.R
+++ R/dplyr.R
@@ -16,43 +16,19 @@ filter.huxtable <- function(.data, ...) {}
 body(filter.huxtable) <- body(filter_.huxtable)
 
 
-#' Dplyr verbs for huxtable
-#'
-#' Huxtable can be used with dplyr verbs \code{\link[dplyr]{select}}, \code{\link[dplyr]{rename}},
-#' \code{\link[dplyr]{slice}}, \code{\link[dplyr]{arrange}}, \code{\link[dplyr]{mutate}} and
-#' \code{\link[dplyr]{transmute}}. These will return huxtables. Other verbs like \code{\link[dplyr]{summarize}}
-#' will simply return data frames as normal.
-#'
-#' @param .data A huxtable.
-#' @param ...,.dots Arguments passed to \code{\link[dplyr]{mutate}}.
-#' @param copy_cell_props Logical: copy cell and column properties from existing columns.
-#'
-#' @details
-#' If \code{mutate} creates new columns, and the argument \code{copy_cell_props} is missing or \code{TRUE}, then cell
-#' and column properties will be copied from existing columns to their left, if there are any. Otherwise, they will be the
-#' standard defaults. Row and table properties, and properties of cells in existing columns, remain unchanged.
-#'
-#' @rdname dplyr-verbs
-#' @aliases mutate
-#' @examples
-#' ht <- hux(a = 1:5, b = 1:5, c = 1:5, d = 1:5)
-#' bold(ht)[c(1, 3), ] <- TRUE
-#' bold(ht)[, 1] <- TRUE
-#' ht2 <- dplyr::select(ht, b:c)
-#' ht2
-#' bold(ht2)
-#' ht3 <- dplyr::mutate(ht, x = a + b)
-#' ht3
-#' bold(ht3)
-#' ht4 <- dplyr::mutate(ht, x = a + b, copy_cell_props = FALSE)
-#' bold(ht4)
 #' @importFrom dplyr mutate_
 #' @export
 mutate_.huxtable <- function (.data, ..., .dots) {
   ht <- .data
   .data <- as.data.frame(.data)
   copy_cell_props <- TRUE
-  if (! is.null(.dots$copy_cell_props)) copy_cell_props <- lazyeval::lazy_eval(.dots$copy_cell_props)
+  if (! is.null(.dots$copy_cell_props)) {
+    if (utils::packageVersion("dplyr") > "0.5.0") {
+      copy_cell_props <- .dots$copy_cell_props
+    } else {
+      copy_cell_props <- lazyeval::lazy_eval(.dots$copy_cell_props)
+    }
+  }
   .dots <- .dots[setdiff(names(.dots), 'copy_cell_props')]
   result <- NextMethod()
   result <- as_hux(result)
@@ -75,7 +51,36 @@ mutate_.huxtable <- function (.data, ..., .dots) {
   result
 }
 
-
+#' Dplyr verbs for huxtable
+#'
+#' Huxtable can be used with dplyr verbs \code{\link[dplyr]{select}}, \code{\link[dplyr]{rename}},
+#' \code{\link[dplyr]{slice}}, \code{\link[dplyr]{arrange}}, \code{\link[dplyr]{mutate}} and
+#' \code{\link[dplyr]{transmute}}. These will return huxtables. Other verbs like \code{\link[dplyr]{summarize}}
+#' will simply return data frames as normal.
+#'
+#' @param .data A huxtable.
+#' @param ... Arguments passed to \code{\link[dplyr]{mutate}}.
+#' @param copy_cell_props Logical: copy cell and column properties from existing columns.
+#'
+#' @details
+#' If \code{mutate} creates new columns, and the argument \code{copy_cell_props} is missing or \code{TRUE}, then cell
+#' and column properties will be copied from existing columns to their left, if there are any. Otherwise, they will be the
+#' standard defaults. Row and table properties, and properties of cells in existing columns, remain unchanged.
+#'
+#' @rdname dplyr-verbs
+#' @aliases mutate
+#' @examples
+#' ht <- hux(a = 1:5, b = 1:5, c = 1:5, d = 1:5)
+#' bold(ht)[c(1, 3), ] <- TRUE
+#' bold(ht)[, 1] <- TRUE
+#' ht2 <- dplyr::select(ht, b:c)
+#' ht2
+#' bold(ht2)
+#' ht3 <- dplyr::mutate(ht, x = a + b)
+#' ht3
+#' bold(ht3)
+#' ht4 <- dplyr::mutate(ht, x = a + b, copy_cell_props = FALSE)
+#' bold(ht4)
 #' @importFrom dplyr mutate
 #' @export
 mutate.huxtable <- function (.data, ..., copy_cell_props = TRUE) {
