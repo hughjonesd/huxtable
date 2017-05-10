8716c0fb841ce53d85219c16150bd4db22836c74
David Hugh-Jones <davidhughjones@gmail.com>
2017-05-10 20:12:15 +0100

    exclude make_getter_setters from coverage testing

--- run-covr.R
+++ run-covr.R
@@ -2,8 +2,8 @@
 devtools::install_github('hughjonesd/huxtable') # paradoxically, travis doesn't install huxtable
 Sys.setenv(COVERAGE="yes")
 all_created <- c(huxtable:::huxtable_cell_attrs, huxtable:::huxtable_col_attrs,
-  huxtable:::huxtable_row_attrs, huxtable:::huxtable_table_attrs)
+  huxtable:::huxtable_row_attrs, huxtable:::huxtable_table_attrs, 'make_getter_setters')
 all_created <- paste0("^", all_created)
-cov <- covr::package_coverage(function_exclusions = all_created)
-covr::codecov(coverage = cov)
+cov_result <- covr::package_coverage(function_exclusions = all_created)
+covr::codecov(coverage = cov_result)
 Sys.unsetenv("COVERAGE")
