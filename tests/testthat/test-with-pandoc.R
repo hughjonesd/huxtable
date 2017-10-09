19089960e65573e3a33c4625acd4dd5d98366821
David Hugh-Jones <davidhughjones@gmail.com>
2017-10-09 14:57:16 +0100

    add omnibus HTML test in test-with-pandoc.R

--- tests/testthat/test-with-pandoc.R
+++ tests/testthat/test-with-pandoc.R
@@ -14,15 +14,23 @@ test_that('Four spaces does not cause <pre><code> markup', {
 })
 
 
-test_that('Row heights do not screw up latex multicol', {
+test_that('Row heights do not screw up LaTeX multicol', {
   skip_without_pandoc()
   on.exit(if (exists('output')) file.remove(output))
   expect_silent(output <- rmarkdown::render('rowheight-multicol-test.Rmd', quiet = TRUE))
 })
 
 
-test_that('table-tester-2.Rmd can be rendered OK', {
+test_that('table-tester-2.Rmd renders without errors in LaTeX', {
   skip_without_pandoc()
   on.exit(if (exists('output')) file.remove(output))
-  expect_silent(output <- rmarkdown::render('table-tester-2.Rmd', quiet = TRUE))
+  expect_silent(output <- rmarkdown::render('table-tester-2.Rmd', quiet = TRUE, output_format = "pdf_document"))
+})
+
+
+
+test_that('table-tester-2.Rmd renders without errors in HTML', {
+  skip_without_pandoc()
+  on.exit(if (exists('output')) file.remove(output))
+  expect_silent(output <- rmarkdown::render('table-tester-2.Rmd', quiet = TRUE, output_format = "html_document"))
 })
