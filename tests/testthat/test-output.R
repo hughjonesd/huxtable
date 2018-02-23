afcfa917c133918eed8d822e7394026e4f4ab4c7
David Hugh-Jones <davidhughjones@gmail.com>
2018-02-23 15:26:05 +0000

    skip quick_pdf on appveyor - installing latex is hard

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -131,10 +131,6 @@ test_that('Quick output functions work', {
   m <- matrix(1:4, 2, 2)
   dfr <- data.frame(a = 1:5, b = 1:5)
 
-  tf <- tempfile(fileext = '.pdf')
-  expect_silent(quick_pdf(m, dfr, ht, file = tf))
-  expect_true(file.exists(tf))
-
   tf <- tempfile(fileext = '.htm')
   expect_silent(quick_html(m, dfr, ht, file = tf))
   expect_true(file.exists(tf))
@@ -148,9 +144,19 @@ test_that('Quick output functions work', {
   expect_true(file.exists(tf))
 })
 
+test_that('quick_pdf works', {
+  skip_on_appveyor()
+  ht <- hux(a = 1:2, b = 1:2)
+  m <- matrix(1:4, 2, 2)
+  dfr <- data.frame(a = 1:5, b = 1:5)
+  tf <- tempfile(fileext = '.pdf')
+  expect_silent(quick_pdf(m, dfr, ht, file = tf))
+  expect_true(file.exists(tf))
+})
+
 
 test_that('Quick output functions stop if called non-interactively with no `file` argument', {
   ht <- hux(a = 1:2, b = 1:2)
-  expect_error(quick_pdf(ht))
-  expect_false(file.exists('huxtable-output.pdf'))
+  expect_error(quick_html(ht))
+  expect_false(file.exists('huxtable-output.html'))
 })
