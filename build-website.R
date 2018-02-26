ce46ad85112242110e01f689608731516437ea11
David Hugh-Jones <davidhughjones@gmail.com>
2018-02-26 11:26:28 +0000

    give up on versioning hassle in docs

--- build-website.R
+++ build-website.R
@@ -1,21 +1,7 @@
 #!/usr/local/bin/Rscript
 
-
 # script to rebuild website files
 
-# replace the below with the tag you want:
-version_tag <- "v3.0.0-rc1"
-
-library(git2r)
-repo <- repository()
-if (! version_tag %in% (nms <- names(tags()))) stop("version_tag must be one of", nms)
-# we will build from this version:
-tag_obj <- tags()[[version_tag]]
-checkout(tag_obj)
-detach('package:git2r') # stops it interfering with psych functions
-devtools::install()
-# checkout master again so our changes in docs will apply to that branch
-git2r::checkout(repo, "master")
 
 for (f in list.files("docs", pattern = "*.Rmd", full.names = TRUE)) {
   message("Rendering ", f)
@@ -24,15 +10,7 @@ for (f in list.files("docs", pattern = "*.Rmd", full.names = TRUE)) {
 }
 
 knitr::knit("docs/index.Rhtml", "docs/index.html")
-
-# back to the tagged version to build the reference with pkgdown
-git2r::checkout(tag_obj)
 pkgdown::build_reference()
 pkgdown::build_reference_index()
 pkgdown::build_news()
-
-library(git2r)
-git2r::stash(repo, untracked = TRUE)
-git2r::checkout(repo, "master")
-merge(repo, pkgdown_commit)
-
+message("Now commit and push to github. Don't forget to reinstall the dev version!")
