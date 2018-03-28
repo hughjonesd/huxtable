3e24a5c3a44beb81a350681056a30998bf4bfb7f
David Hugh-Jones <davidhughjones@gmail.com>
2018-03-28 10:16:56 +0100

    Many new output tests

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -115,14 +115,60 @@ test_that('guess_knitr_output_format() gets it right', {
 })
 
 
+expect_outputs_unchanged <- function (hx, idx) {
+  info <- paste0("Index i = ", idx)
+  file <- file.path("example-rds", paste0("various-outputs-", idx))
+  expect_known_value(to_screen(hx), file = paste0(file, "-screen.rds"), info = info)
+  expect_known_value(to_md(hx),     file = paste0(file, "-md.rds"),     info = info)
+  expect_known_value(to_html(hx),   file = paste0(file, "-html.rds"),   info = info)
+  expect_known_value(to_latex(hx),  file = paste0(file, "-latex.rds"),  info = info)
+}
+
+test_that('various outputs unchanged', {
+  hx <- hux(
+          int  = 1:3,
+          real = 1:3 + 0.005,
+          char = letters[1:3],
+          date = as.Date(1:3, origin = "1970-01-01"),
+          fact = factor(letters[4:6])
+        )
+  variations <- expand.grid(
+          align             = c('left', 'right', 'centre', '.'),
+          background_color  = c('red', grey(.6), NA),
+          bold              = c(TRUE, FALSE),
+          escape_contents   = c(TRUE, FALSE),
+          font_size         = c(8, 10),
+          italic            = c(TRUE, FALSE),
+          left_border       = c(0, 1, 2),
+          left_border_color = c('red', grey(.6)),
+          left_padding      = c(0, 4),
+          number_format     = c("%3.1g", NA),
+          rotation          = c(0, 90),
+          text_color        = c('red', grey(.6)),
+          valign            = c('top', 'middle', 'bottom'),
+          wrap              = c(TRUE, FALSE),
+          stringsAsFactors  = FALSE,
+          KEEP.OUT.ATTRS   = FALSE
+        )
+
+  set.seed(271075L) # expect_unchanged is useless if we always pick new variations
+  for (i in sample(nrow(variations), 300)) {
+    props <- as.list(variations[i,])
+    props$ht <- hx
+    hx_set <- do.call("set_cell_properties", props)
+    expect_outputs_unchanged(hx_set, i)
+  }
+})
+
 test_that('set_print_method() works', {
   ht <- hux(a = 1:2, b = 1:2)
-  options(huxtable.print = print_html)
+  oo <- options(huxtable.print = print_html)
   expect_match(capture.output(print(ht)), '<table', fixed = TRUE, all = FALSE)
   options(huxtable.print = print_latex)
   expect_match(capture.output(print(ht)), 'tabular', fixed = TRUE, all = FALSE)
   options(huxtable.print = 'print_html')
   expect_match(capture.output(print(ht)), '<table', fixed = TRUE, all = FALSE)
+  options(oo)
 })
 
 
