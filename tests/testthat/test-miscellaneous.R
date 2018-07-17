d71f8159055b2ad443060c0c20bf529859a99f24
David Hugh-Jones <davidhughjones@gmail.com>
2018-07-17 18:08:52 +0100

    Tests for .onLoad registering knitr and dplyr methods

--- tests/testthat/test-miscellaneous.R
+++ tests/testthat/test-miscellaneous.R
@@ -14,9 +14,32 @@ test_that('.onLoad works and sets options', {
           huxtable.autoformat_align = NULL
         )
   options(hux_opts)
-  expect_silent(huxtable:::.onLoad(system.file(package='huxtable'), 'huxtable'))
+  expect_silent(huxtable:::.onLoad(system.file(package = 'huxtable'), 'huxtable'))
   for (opt in names(hux_opts)) {
     expect_false( is.null(options(opt)[[1]]), info = paste("Option:", opt))
   }
   options(old_opts)
+
+
+})
+
+
+# can be run via devtools::test, but not automatically from RStudio
+test_that('.onLoad gets methods registered', {
+  tryCatch(unloadNamespace('dplyr'), error = function (e) skip("Couldn't unload dplyr namespace"))
+
+  huxtable:::.onLoad(system.file(package = 'huxtable'), 'huxtable')
+  expected_methods <- c('arrange', 'arrange_', 'filter', 'filter_', 'mutate', 'mutate_', 'rename',
+    'rename_', 'select', 'select_', 'slice', 'slice_', 'transmute', 'transmute_')
+  dplyr_methods <- getNamespaceInfo('dplyr', 'S3methods')  # will load dplyr
+  dplyr_methods <- as.data.frame(dplyr_methods[, 1:2])
+  names(dplyr_methods) <- c('generic', 'class')
+  dplyr_methods <- dplyr::filter(dplyr_methods, class == 'huxtable')
+  expect_setequal(unlist(dplyr_methods$generic), expected_methods)
+
+  knitr_methods <- getNamespaceInfo('knitr', 'S3methods')
+  knitr_methods <- as.data.frame(knitr_methods[, 1:2])
+  names(knitr_methods) <- c('generic', 'class')
+  knitr_methods <- dplyr::filter(knitr_methods, generic == 'knit_print')
+  expect_true(all(c('huxtable', 'data.frame') %in% unlist(knitr_methods$class)))
 })
