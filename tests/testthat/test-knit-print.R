58a5d688f9cb485b1a41111719a6e29d7a787ada
David Hugh-Jones <davidhughjones@gmail.com>
2018-07-25 20:54:59 +0100

    tests for knitr_output_format option

--- tests/testthat/test-knit-print.R
+++ tests/testthat/test-knit-print.R
@@ -7,7 +7,7 @@ source('functions.R')
 
 test_that("Can turn on and off printing of data frames", {
   expect_huxish <- function (dfr, bool) {
-    tmp <- capture.output(knitr::knit_print(dfr))
+    tmp <- knitr::knit_print(dfr)
     expect_identical(any(grepl("Column names", tmp)), bool)
   }
   dfr <- data.frame(a = 1:3, b = 1:3)
@@ -28,6 +28,29 @@ test_that("Can theme data frames", {
   }
   options(huxtable.knit_print_df_theme = mytheme)
   dfr <- data.frame(a = 1:2, b = 1:2)
-  tmp <- capture.output(knitr::knit_print(dfr))
+  tmp <- knitr::knit_print(dfr)
   expect_true(any(grepl("\\.00000", tmp)))
+  options(oo)
+})
+
+
+test_that("knitr_output_format overrides default output format in knit_print", {
+  oo <- options(huxtable.knitr_output_format = "html")
+  ht <- hux(a = 1)
+  expect_match(knitr::knit_print(ht), "<table")
+  options(huxtable.knitr_output_format = "latex")
+  expect_match(knitr::knit_print(ht), "tabular", fixed = TRUE)
+  options(huxtable.knitr_output_format = "md")
+  expect_match(knitr::knit_print(ht), "---", fixed = TRUE)
+
+  options(oo)
+})
+
+
+test_that("knit_print warns if output format is weird", {
+  oo <- options(huxtable.knitr_output_format = "weirdness")
+  ht <- hux(a = 1)
+  expect_warning(knitr::knit_print(ht), "output format")
+
+  options(oo)
 })
