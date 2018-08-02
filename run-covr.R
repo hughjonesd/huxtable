2f5a248de7eca2e42244cc150c7a8066ac16bc1b
David Hugh-Jones <davidhughjones@gmail.com>
2018-08-02 13:33:37 +0100

    trivia

--- run-covr.R
+++ run-covr.R
@@ -5,5 +5,8 @@ all_created <- c(huxtable:::huxtable_cell_attrs, huxtable:::huxtable_col_attrs,
   huxtable:::huxtable_row_attrs, huxtable:::huxtable_table_attrs, 'make_getter_setters')
 all_created <- paste0("^", all_created)
 cov_result <- covr::package_coverage(function_exclusions = all_created)
+
 covr::codecov(coverage = cov_result)
+# to do it manually, run the below, with token = "your token from https://codecov.io/gh/hughjonesd/huxtable/settings"; then make sure you delete the token!
+# covr::codecov(coverage = cov_result, token = "")
 Sys.unsetenv("COVERAGE")
