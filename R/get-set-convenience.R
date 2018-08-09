fbdde24e0715809142b80955c2e1b84f4ce211c3
David Hugh-Jones <davidhughjones@gmail.com>
2018-08-09 19:49:17 +0100

    drop argument to get_all_*

--- R/get-set-convenience.R
+++ R/get-set-convenience.R
@@ -228,12 +228,12 @@ merge_cells <- function (ht, row, col) {
 }
 
 
-get_all_borders <- function(ht, row, col) {
+get_all_borders <- function(ht, row, col, drop = TRUE) {
   list(
-    left   = left_border(ht)[row, col],
-    right  = right_border(ht)[row, col],
-    top    = top_border(ht)[row, col],
-    bottom = bottom_border(ht)[row, col]
+    left   = left_border(ht)[row, col, drop = drop],
+    right  = right_border(ht)[row, col, drop = drop],
+    top    = top_border(ht)[row, col, drop = drop],
+    bottom = bottom_border(ht)[row, col, drop = drop]
   )
 }
 
@@ -248,11 +248,21 @@ get_all_border_colors <- function(ht, row, col, drop = TRUE) {
 }
 
 
-get_all_border_styles <- function(ht, row, col) {
+get_all_border_styles <- function(ht, row, col, drop = TRUE) {
   list(
-    left   = left_border_style(ht)[row, col],
-    right  = right_border_style(ht)[row, col],
-    top    = top_border_style(ht)[row, col],
-    bottom = bottom_border_style(ht)[row, col]
+    left   = left_border_style(ht)[row, col, drop = drop],
+    right  = right_border_style(ht)[row, col, drop = drop],
+    top    = top_border_style(ht)[row, col, drop = drop],
+    bottom = bottom_border_style(ht)[row, col, drop = drop]
+  )
+}
+
+
+get_all_padding <- function(ht, row, col, drop = TRUE) {
+  list(
+    left   = left_padding(ht)[row, col, drop = drop],
+    right  = right_padding(ht)[row, col, drop = drop],
+    top    = top_padding(ht)[row, col, drop = drop],
+    bottom = bottom_padding(ht)[row, col, drop = drop]
   )
 }
