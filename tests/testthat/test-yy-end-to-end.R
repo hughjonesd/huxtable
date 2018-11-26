0482bd462dc4e3503b297a8a053e440a65abb6c7
David Hugh-Jones <davidhughjones@gmail.com>
2018-11-26 20:25:46 +0000

    bugfix test

--- tests/testthat/test-yy-end-to-end.R
+++ tests/testthat/test-yy-end-to-end.R
@@ -138,11 +138,14 @@ test_that("Various Rmd files render without errors", {
 
 
 test_that("Bookdown files", {
+  skip_if_not_installed("dplyr")
+  skip_if_not_installed("bookdown")
   test_render("bookdown-test.Rmd", "bookdown::pdf_book")
   test_render("bookdown-test.Rmd", "bookdown::html_book", output_dir = ".") # workaround a bug
 })
 
 
 test_that("Word files", {
+  skip_if_not_installed("flextable")
   test_render("word-test.Rmd", "word_document")
 })
