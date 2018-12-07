82dcef677bed2572fd0ea79a505a5a62b7b6e232
David Hugh-Jones <davidhughjones@gmail.com>
2018-12-07 18:04:54 +0000

    fix tests for new devtools

--- tests/testthat/setup-functions.R
+++ tests/testthat/setup-functions.R
@@ -2,18 +2,18 @@
 options(width = 80) # keeps to_screen examples unchanged
 
 example_code_for_topic <- function (fname) {
-  path <- devtools::find_topic(fname)[1] # sometimes we get multiples!
-
-  if (is.null(path)) stop('No help exists for ', fname)
-  expath <- tempfile(paste0('test-example-', fname))
+  path <- help(fname, package = "huxtable") # sometimes we get multiples!
+  path <- path$path
+  if (is.null(path)) stop("No help exists for ", fname)
+  expath <- tempfile(paste0("test-example-", fname))
   tools::Rd2ex(path, expath, commentDontrun = TRUE, commentDonttest = TRUE)
-  if (! file.exists(expath)) stop('Could not make example for ', fname)
+  if (! file.exists(expath)) stop("Could not make example for ", fname)
   parse(expath)
 }
 
 
-code_path_for_topic <- function (fname) file.path(test_path(), 'example-rds',
-      paste0(fname, '-example-code.rds'))
+code_path_for_topic <- function (fname) file.path(test_path(), "example-rds",
+      paste0(fname, "-example-code.rds"))
 
 
 # runs example and checks output & values haven't changed; optionally reset files if code has changed
@@ -24,7 +24,7 @@ test_ex_same <- function(fname, reset_on_change = TRUE) {
   if (file.exists(codepath)) {
     comp <- testthat::compare(deparse(excode), readRDS(codepath))
     if (! comp$equal) {
-      warning('Example code for ', fname, ' changed')
+      warning("Example code for ", fname, " changed")
       if (reset_on_change) reset_example_test(fname)
       return(invisible())
     }
@@ -36,12 +36,12 @@ test_ex_same <- function(fname, reset_on_change = TRUE) {
   i <- 1
   for (expr in excode) {
     out <- capture.output(res <- eval(expr, envir = exenv))
-    rds_name <- paste0(fname, '-example-value-', i, '.rds')
-    fp <- file.path(testthat::test_path(), 'example-rds', rds_name)
+    rds_name <- paste0(fname, "-example-value-", i, ".rds")
+    fp <- file.path(testthat::test_path(), "example-rds", rds_name)
     expect_equal_to_reference(res, fp, info =
           paste0('In "', fname, '", expression changed value: ', deparse(expr, nlines = 1)))
-    rds_name <- paste0(fname, '-example-output-', i, '.rds')
-    fp <- file.path(testthat::test_path(), 'example-rds', rds_name)
+    rds_name <- paste0(fname, "-example-output-", i, ".rds")
+    fp <- file.path(testthat::test_path(), "example-rds", rds_name)
     expect_equal_to_reference(out, fp, info =
           paste0('In "', fname, '", expression changed output: ', deparse(expr, nlines = 1)))
 
@@ -51,12 +51,12 @@ test_ex_same <- function(fname, reset_on_change = TRUE) {
 
 
 reset_example_test <- function (fnames) {
-  devtools::load_all('.')
+  devtools::load_all(".")
 
   for (fname in fnames) {
     # get rid of output and code files
     fs <- list.files(file.path(testthat::test_path()))
-    fs <- grep(paste0('^', fname, '-example-.*'), fs, value = TRUE)
+    fs <- grep(paste0("^", fname, "-example-.*"), fs, value = TRUE)
     fs <- file.path(testthat::test_path(), fs)
     sapply(fs, file.remove)
     remake_code_file(fname)
@@ -75,21 +75,21 @@ remake_code_file <- function (fname) {
 
 
 skip_without_pandoc <- function () {
-  if (! rmarkdown::pandoc_available('1.12.3')) skip('Not testing, pandoc >= 1.12.3 is not available')
+  if (! rmarkdown::pandoc_available("1.12.3")) skip("Not testing, pandoc >= 1.12.3 is not available")
 }
 
 
 skip_on_R_CMD_check <- function () {
-  skip_if(exists('we_are_in_R_CMD_check'), 'Not testing, code doesn\'t play well with R CMD check')
-  skip_if(Sys.getenv('COVERAGE') != '', 'Not testing, code doesn\'t play well with R CMD check')
+  skip_if(exists("we_are_in_R_CMD_check"), "Not testing, code doesn't play well with R CMD check")
+  skip_if(Sys.getenv("COVERAGE") != "", "Not testing, code doesn't play well with R CMD check")
 }
 
 
 require_temp_artefacts_dir <- function () {
-  if (dir.exists('temp-artefacts')) return(TRUE)
-  if (dir.create('temp-artefacts', showWarnings = FALSE)) {
+  if (dir.exists("temp-artefacts")) return(TRUE)
+  if (dir.create("temp-artefacts", showWarnings = FALSE)) {
     return(TRUE)
   } else {
-    skip('Couldn\'t create temp-artefacts dir')
+    skip("Couldn't create temp-artefacts dir")
   }
 }
