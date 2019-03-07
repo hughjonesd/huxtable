f65f4cd6a202a0f588af946d256dbb60aa0210f4
David Hugh-Jones <davidhughjones@gmail.com>
2019-03-07 17:00:49 +0000

    remove script with private token from git

--- delete-appveyor-cache.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export APPVEYOR_TOKEN="put your token here"
-curl -X "DELETE" -H "Authorization: Bearer $APPVEYOR_TOKEN" -H "Content-Type: application/json" https://ci.appveyor.com/api/projects/hughjonesd/huxtable/buildcache
