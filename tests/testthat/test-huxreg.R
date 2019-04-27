c0e3745516e09b043a000afb1512300be8ae7124
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2019-04-27 16:19:48 -0500

    Fix mis-specified warning test

--- tests/testthat/test-huxreg.R
+++ tests/testthat/test-huxreg.R
@@ -248,5 +248,5 @@ test_that("tidy.tidy_override works if underlying object has no tidy() method", 
 
 test_that("test huxreg will produce warnings for invalid error_formats",{
     lm1 <- lm(Sepal.Width ~ Sepal.Length, data = iris)
-    expect_warning(hr <- huxreg(lm1), regexp = NA)
+    expect_warning(hr <- huxreg(lm1, error_format="({std.error}){nonstandard.error}"))
 })
