21ca63a5ee18d2ca4395a6cf1bec4126f86573e4
David Hugh-Jones <davidhughjones@gmail.com>
2019-10-03 22:10:30 +0100

    Make example templates work together

--- man-roxygen/getset-example.R
+++ man-roxygen/getset-example.R
@@ -4,6 +4,8 @@
 <% if (! exists('subscript')) subscript <- '' %>
 #' @examples
 #'
+#' orig <- <%= attr_name %>(jams)
 #' <%= attr_name %>(jams)<%= subscript %> <-  <%= attr_val %>
 #' <%= attr_name %>(jams)
 #' <%= if (exists('extra')) gsub('## *', '\n#\' ', extra) %>
+#' <%= attr_name %>(jams) <- orig
