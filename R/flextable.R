14ad0a2a73ce988d3bddaa8e6bb9d8cfd2660040
Sebastian Jeworutzki <sebastian.jeworutzki@ruhr-uni-bochum.de>
2019-12-04 10:43:57 +0100

    Check for flextable >= 0.5.5 and give note to user

--- R/flextable.R
+++ R/flextable.R
@@ -120,7 +120,11 @@ as_flextable.huxtable <- function(x, colnames_to_header = FALSE, ...) {
 
   # set caption
   if(!is.null(caption(x)) & !is.na(caption(x))) {
-  ft <- flextable::set_caption(ft, caption(x))
+    if(packageVersion("flextable") >= "0.5.5"){
+      ft <- flextable::set_caption(ft, caption(x))
+    } else {
+      message('Use of table captions requires package "flextable" => 0.5.5.\nType:\n install.packages("flextable")')
+    }
   }
 
   ft
