9bd8368637f4d65ec294b80c026848199d783244
David Hugh-Jones <davidhughjones@gmail.com>
2020-05-02 13:35:31 +0100

    bugfix example and vignette for add_colnames = TRUE

--- R/dplyr.R
+++ R/dplyr.R
@@ -63,7 +63,7 @@ mutate_.huxtable <- function (.data, ..., .dots) {
 #' @rdname dplyr-verbs
 #' @aliases mutate dplyr-verbs
 #' @examples
-#' ht <- hux(a = 1:5, b = 1:5, c = 1:5, d = 1:5)
+#' ht <- hux(a = 1:5, b = 1:5, c = 1:5, d = 1:5, add_colnames = FALSE)
 #' bold(ht)[c(1, 3), ] <- TRUE
 #' bold(ht)[, 1] <- TRUE
 #' ht2 <- dplyr::select(ht, b:c)
