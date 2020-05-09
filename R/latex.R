309872f1064efe676030db34204ec5e0f38d7ff8
David Hugh-Jones <davidhughjones@gmail.com>
2020-05-09 08:14:15 +0100

    bugfix latex caption_width

--- R/latex.R
+++ R/latex.R
@@ -79,7 +79,7 @@ to_latex.huxtable <- function (ht, tabular_only = FALSE, ...){
       center = "centering",
       right  = "raggedleft"
     )
-    if (is.numeric(cap_width)) cap_width <- sprintf("%s\\textwidth", cap_width)
+    if (! is.na(as.numeric(cap_width))) cap_width <- sprintf("%s\\textwidth", cap_width)
     cap_width_tex <- if (! is.na(cap_width))  sprintf(",width=%s", cap_width) else ""
     sprintf("\\captionsetup{justification=%s,singlelinecheck=off%s}\n\\caption{%s}\n",
           cap_setup, cap_width_tex, cap)
