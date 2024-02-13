508c8aaf67eb9dad4823eab0d9566c2b72bc52e8
David Hugh-Jones <davidhughjones@gmail.com>
2024-02-13 09:15:52 +0000

    Don't warn about quarto chunk labels if label starts with "unnamed-chunk"

--- R/aaa-utils.R
+++ R/aaa-utils.R
@@ -212,6 +212,8 @@ make_label <- function (ht) {
 
   has_knitr <- requireNamespace("knitr", quietly = TRUE)
   chunk_label <- if (has_knitr) knitr::opts_current$get("label") else NULL
+  if (grepl("^unnamed-chunk", chunk_label)) chunk_label <- NULL
+
   if (is.na(lab) &&
       getOption("huxtable.autolabel", TRUE) &&
       has_knitr &&
