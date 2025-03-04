b4312aa860d12c1c4d2547488f61cfcc35539b43
David Hugh-Jones <davidhughjones@gmail.com>
2025-03-04 18:30:10 +0000

    Tweak pkgdown and build scripts

--- build-cran.R
+++ build-cran.R
@@ -89,7 +89,7 @@ tags <- as.numeric(gsub(newtag, '', tags))
 rc <- if (length(tags)) as.integer(max(tags) + 1) else 1L
 newtag <- paste0(newtag, rc)
 cat('\nTagging current version as: ', newtag, '\n')
-tag(repository('.'), newtag, message = paste('CRAN release candidate for', v))
+git2r::tag(git2r::repository('.'), newtag, message = paste('CRAN release candidate for', v))
 system2('git', c('push', '--tags'))
 
 
