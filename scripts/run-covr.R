69099ccb29081e9e8d6b08931e91171091d64c17
David Hugh-Jones <davidhughjones@gmail.com>
2025-08-14 12:00:15 +0100

    Remove little-used scripts files

--- scripts/run-covr.R
+++
@@ -1,9 +0,0 @@
-
-library(covr)
-library(huxtable)
-Sys.setenv(COVERAGE='yes')
-all_created <- c(huxtable:::huxtable_cell_attrs, huxtable:::huxtable_col_attrs,
-      huxtable:::huxtable_row_attrs, huxtable:::huxtable_table_attrs)
-all_created <- paste0('^', all_created)
-cov <- package_coverage(function_exclusions = all_created)
-Sys.unsetenv('COVERAGE')
